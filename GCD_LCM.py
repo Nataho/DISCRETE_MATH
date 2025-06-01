0c926c11cf52b2dcdd5f86a37c525f29cc429a0b
Nataho <153593932+Nataho@users.noreply.github.com>
2025-06-02 05:22:16 +0800

    1.01

--- GCD_LCM.py
+++ GCD_LCM.py
@@ -4,37 +4,46 @@ from Prime import Prime
 class GCD_LCM():
     def __init__(self):
         TOOLS.clear_screen()
-        self.GCD_methods = [
-            "Euclidean algorithm",
+        self.methods = [
+            "(GCD) Euclidean algorithm",
+            "(LCM) Using GCD",
             # "Prime factorization",
             # "Binary GCD algorithm",
-        ]
-        self.LCM_methods = [
-            # "Using GCD",
-            # "Prime factorization",
-            # "Listing multiples",
         ]
         self.start()
 
     def start(self):
         TOOLS.print_type("Pick a method to calculate GCD:")
-        for i, method in enumerate(self.GCD_methods):
+        for i, method in enumerate(self.methods):
             TOOLS.print_type(f"{i + 1}. {method}")
         choice = int(TOOLS.input_type("Enter the number of your choice: ")) - 1
-        if choice < 0 or choice >= len(self.GCD_methods):
+        if choice < 0 or choice >= len(self.methods):
             TOOLS.print_type("Invalid choice. Please try again.")
             return
-        method_name = self.GCD_methods[choice]
+        method_name = self.methods[choice]
         TOOLS.print_type("")
         TOOLS.print_type(f"You chose: {method_name}")
 
-        if method_name == "Euclidean algorithm":
-            a = int(TOOLS.input_type("Enter first number (a): "))
-            b = int(TOOLS.input_type("Enter second number (b): "))
-            result = self.eucledian(a, b)
-            TOOLS.sleep(0.5)
-            print()
-            TOOLS.print_type(f"GCD({a}, {b}) = {result}")
+        match method_name:
+            case "(GCD) Euclidean algorithm":
+                a = int(TOOLS.input_type("Enter first number (a): "))
+                b = int(TOOLS.input_type("Enter second number (b): "))
+                result = self.eucledian(a, b)
+                TOOLS.sleep(0.5)
+                print()
+                TOOLS.print_type(f"GCD({a}, {b}) = {result}")
+            case "(LCM) Using GCD":
+                a = int(TOOLS.input_type("Enter first number (a): "))
+                b = int(TOOLS.input_type("Enter second number (b): "))
+                gcd = self.eucledian(a, b)
+                TOOLS.sleep(0.5)
+                print()
+                TOOLS.print_type(f"GCD({a}, {b}) = {gcd}")
+                TOOLS.print_type(f"LCM({a}, {b}) = (a * b) // GCD(a, b)")
+                TOOLS.print_type(f"LCM({a}, {b}) = ({a} * {b}) // {gcd}")
+                lcm = (a * b) // gcd
+                TOOLS.print_type(f"LCM({a}, {b}) = {lcm}")
+
 
     def eucledian(self, a, b):
         """Calculate GCD using the Euclidean algorithm."""
@@ -43,4 +52,5 @@ class GCD_LCM():
             TOOLS.print_type(f"{a} = {b}({a // b}) + {a % b}")
             a, b = b, a % b #swap
         return a
+
     # def
